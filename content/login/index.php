7bc3eb6b2ab1d41bce4a19ce4faa5f9948967fc2
TomoakiMacBookProM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-11-24 23:25:41 +0900

    New: content > login > index.php, index.phtml

---
+++ content/login/index.php
@@ -0,0 +1,21 @@
+<?php
+/**	op-module-public_html-onepiece:/content/login/index.php
+ *
+ * @created   2025-11-24
+ * @version   1.0
+ * @package   op-module-public_html-onepiece
+ * @author    Tomoaki Nagahara
+ * @copyright Tomoaki Nagahara All rights reserved.
+ */
+
+/**	declare
+ *
+ */
+declare(strict_types=1);
+
+/**	namespace
+ *
+ */
+namespace OP;
+
+OP()->Unit('Login')->Auto();
